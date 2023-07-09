ff4e450a10d2342b51e5603a1c0ae41660d7efe0
Michiel van Oosterhout <michielvoo@users.noreply.github.com>
2023-07-09 19:39:14 +0300

    Install and import module using Visual Studio Code task

--- .vscode/tasks.ps1
+++ .vscode/tasks.ps1
@@ -12,4 +12,26 @@ switch ($Task) {
     "Test" {
         Invoke-Pester ${workspaceFolder}
     }
+
+    "Install" {
+        $InformationPreference = "Continue"
+
+        $installedModule = Get-InstalledModule Pilot -ErrorAction SilentlyContinue
+        if ($installedModule) {
+            Uninstall-Module Pilot -ErrorAction Stop
+            Write-Information "Uninstalled module"
+        }
+
+        $repository = Get-PSRepository Pilot -ErrorAction SilentlyContinue
+        if ($repository) {
+            $module = Find-Module Pilot -Repository Pilot -AllowPrerelease -ErrorAction SilentlyContinue
+            Write-Information "Found module"
+            if ($module) {
+                Install-Module Pilot -Repository Pilot -AllowPrerelease -ErrorAction Stop
+                Write-Information "Installed module"
+                Import-Module Pilot -ErrorAction Stop
+                Write-Information "Imported module"
+            }
+        }
+    }
 }
