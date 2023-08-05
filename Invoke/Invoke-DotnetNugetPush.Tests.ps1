a4bfebe5e7d14c0865660593e9e2caa1262010b6
Michiel van Oosterhout <michielvoo@users.noreply.github.com>
2023-08-05 21:03:20 +0300

    Add parameter aliases to Invoke-DotnetNugetPush

--- Invoke/Invoke-DotnetNugetPush.Tests.ps1
+++ Invoke/Invoke-DotnetNugetPush.Tests.ps1
@@ -8,16 +8,37 @@ BeforeAll {
 Describe Invoke-DotnetNugetPush {
     It "invokes dotnet nuget push with arguments" {
         # Act
-        Invoke-DotnetNugetPush "1" -ApiKey "2" -SkipDuplicate -Source "3"
+        $parameters = @{
+            ApiKey = 2
+            DisableBuffering = $true
+            ForceEnglishOutput = $true
+            Interactive = $true
+            NoServiceEndpoint = $true
+            NoSymbols = $true
+            SkipDuplicate = $true
+            Source = 3
+            SymbolApiKey = 4
+            SymbolSource = 5
+            Timeout = 6
+        }
+        Invoke-DotnetNugetPush 1 @parameters
 
         # Assert
         Should -Invoke Invoke-DotnetNuGet -ParameterFilter {
             $Command | Should -BeExactly "push"
             $Arguments | Should -Be @(
-                "1"
-                "--api-key", "2"
+                1
+                "--api-key", 2
+                "--disable-buffering"
+                "--force-english-output"
+                "--interactive"
+                "--no-service-endpoint"
+                "--no-symbols"
                 "--skip-duplicate"
-                "--source", "3"
+                "--source", 3
+                "--symbol-api-key", 4
+                "--symbol-source", 5
+                "--timeout", 6
             )
             $true
         }
