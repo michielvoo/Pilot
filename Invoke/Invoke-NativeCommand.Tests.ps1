1c8d7b496cc244b37d567917127d661792cec2de
Michiel van Oosterhout <michielvoo@users.noreply.github.com>
2023-08-05 10:25:20 +0300

    Cmdlet to invoke a native command and capture output

--- Invoke/Invoke-NativeCommand.Tests.ps1
+++ Invoke/Invoke-NativeCommand.Tests.ps1
@@ -13,14 +13,64 @@ BeforeAll {
 }
 
 Describe Invoke-NativeCommand {
-    It "invokes output" {
+    It "invokes native command" {
         # Act
-        $result = Invoke-NativeCommand $output --stdout a --stdout b --stdout c --exit 12
+        $result = Invoke-NativeCommand $output --stdout,test,--exit,12
 
         # Assert
         $LASTEXITCODE | Should -Be 12
-        $result[0] | Should -BeExact "a"
-        $result[1] | Should -BeExact "b"
-        $result[2] | Should -BeExact "c"
+        $result | Should -Be $null
+    }
+
+    It "appends the native command's stdout line to -Stdout" {
+        # Act
+        $stdout = @()
+        Invoke-NativeCommand $output --stdout,output ([ref]$stdout)
+
+        # Assert
+        $stdout -is [array] | Should -BeTrue
+        $stdout.Count | Should -Be 1
+        $stdout[0] | Should -BeOfType [string]
+        $stdout[0] | Should -BeExactly "output"
+    }
+
+    It "appends the native command's stdout lines to -Stdout" {
+        # Act
+        $stdout = @()
+        Invoke-NativeCommand $output --stdout,1,--stdout,2 ([ref]$stdout)
+
+        # Assert
+        $stdout -is [array] | Should -BeTrue
+        $stdout.Count | Should -Be 2
+        $stdout[0] | Should -BeOfType [string]
+        $stdout[0] | Should -BeExactly "1"
+        $stdout[1] | Should -BeOfType [string]
+        $stdout[1] | Should -BeExactly "2"
+    }
+
+    It "appends the native command's stderr line to -Stderr as a [string]" {
+        # Act
+        $stderr = @()
+        Invoke-NativeCommand $output --stderr,error -Stderr ([ref]$stderr)
+
+        # Assert
+        $stderr -is [array] | Should -BeTrue
+        $stderr.Count | Should -Be 1
+        $stderr[0] | Should -BeOfType [string]
+        $stderr[0] | Should -BeExactly "error"
+    }
+
+    It "appends the native command's stderr lines to -Stdout as [string]s" {
+        # Act
+        $stderr = @()
+        Invoke-NativeCommand $output --stderr,1,--stderr,2 -Stderr ([ref]$stderr)
+
+        # Assert
+        $stderr -is [array] | Should -BeTrue
+        $stderr.Count | Should -Be 2
+        $stderr[0] | Should -BeOfType [string]
+        $stderr[0] | Should -BeExactly "1"
+        $stderr[1] | Should -BeOfType [string]
+        $stderr[1] | Should -BeExactly "2"
     }
 }
--- Invoke/Invoke-NativeCommand.ps1
+++ Invoke/Invoke-NativeCommand.ps1
@@ -4,9 +4,33 @@ function Invoke-NativeCommand {
         [Parameter(Mandatory, Position = 0)]
         [string] $LiteralPath,
 
-        [Parameter(ValueFromRemainingArguments)]
-        [string[]] $Arguments
+        [Parameter(Mandatory, Position = 1)]
+        [string[]] $Arguments,
+
+        [AllowEmptyCollection()]
+        [Parameter(Position = 2)]
+        [ValidateScript({ $_.Value -is [array] -and $_.Value.Count -eq 0 })]
+        [ref] $Stdout,
+
+        [AllowEmptyCollection()]
+        [Parameter(Position = 3)]
+        [ValidateScript({ $_.Value -is [array] -and $_.Value.Count -eq 0 })]
+        [ref] $Stderr
     )
 
-    & $LiteralPath @Arguments
+    $output = & $LiteralPath @Arguments 2>&1
+
+    if ($null -eq $output) {
+        return
+    }
+
+    foreach ($line in $output) {
+        if ($null -ne $Stdout -and $line -is [string]) {
+            $Stdout.Value += $line
+        }
+
+        if ($null -ne $Stderr -and $line -is [System.Management.Automation.ErrorRecord]) {
+            $Stderr.Value += $line.TargetObject
+        }
+    }
 }
