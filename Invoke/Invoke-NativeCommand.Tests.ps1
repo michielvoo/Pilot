d7cb896c794fa159080545eb3381df4990d03a81
Michiel van Oosterhout <michielvoo@users.noreply.github.com>
2023-08-05 13:01:57 +0300

    Return exit code, stdout, and stderr from native command

--- Invoke/Invoke-NativeCommand.Tests.ps1
+++ Invoke/Invoke-NativeCommand.Tests.ps1
@@ -13,64 +13,60 @@ BeforeAll {
 }
 
 Describe Invoke-NativeCommand {
-    It "invokes native command" {
+    It "invokes native command and returns [hashtable] with native command's exit code" {
         # Act
-        $result = Invoke-NativeCommand $output --stdout,test,--exit,12
+        $result = Invoke-NativeCommand $output --exit 12
 
         # Assert
-        $LASTEXITCODE | Should -Be 12
-        $result | Should -Be $null
+        $result | Should -BeOfType [hashtable]
+        $result.ExitCode | Should -Be 12
     }
 
-    It "appends the native command's stdout line to -Stdout" {
+    It "returns a single stdout line from the native command as an array" {
         # Act
-        $stdout = @()
-        Invoke-NativeCommand $output --stdout,output ([ref]$stdout)
+        $result = Invoke-NativeCommand $output --stdout out
 
         # Assert
-        $stdout -is [array] | Should -BeTrue
-        $stdout.Count | Should -Be 1
-        $stdout[0] | Should -BeOfType [string]
-        $stdout[0] | Should -BeExactly "output"
+        $result.Stdout -is [array] | Should -BeTrue
+        $result.Stdout.Count | Should -Be 1
+        $result.Stdout[0] | Should -BeOfType [string]
+        $result.Stdout[0] | Should -BeExactly "out"
     }
 
-    It "appends the native command's stdout lines to -Stdout" {
+    It "returns multiple stdout lines from the native command as an array" {
         # Act
-        $stdout = @()
-        Invoke-NativeCommand $output --stdout,1,--stdout,2 ([ref]$stdout)
+        $result = Invoke-NativeCommand $output --stdout out1 --stdout out2
 
         # Assert
-        $stdout -is [array] | Should -BeTrue
-        $stdout.Count | Should -Be 2
-        $stdout[0] | Should -BeOfType [string]
-        $stdout[0] | Should -BeExactly "1"
-        $stdout[1] | Should -BeOfType [string]
-        $stdout[1] | Should -BeExactly "2"
+        $result.Stdout -is [array] | Should -BeTrue
+        $result.Stdout.Count | Should -Be 2
+        $result.Stdout[0] | Should -BeOfType [string]
+        $result.Stdout[0] | Should -BeExactly "out1"
+        $result.Stdout[1] | Should -BeOfType [string]
+        $result.Stdout[1] | Should -BeExactly "out2"
     }
 
-    It "appends the native command's stderr line to -Stderr as a [string]" {
+    It "returns a single stderr line from the native command as an array" {
         # Act
-        $stderr = @()
-        Invoke-NativeCommand $output --stderr,error -Stderr ([ref]$stderr)
+        $result = Invoke-NativeCommand $output --stderr err
 
         # Assert
-        $stderr -is [array] | Should -BeTrue
-        $stderr.Count | Should -Be 1
-        $stderr[0] | Should -BeOfType [string]
-        $stderr[0] | Should -BeExactly "error"
+        $result.Stderr -is [array] | Should -BeTrue
+        $result.Stderr.Count | Should -Be 1
+        $result.Stderr[0] | Should -BeOfType [string]
+        $result.Stderr[0] | Should -BeExactly "err"
     }
 
-    It "appends the native command's stderr lines to -Stdout as [string]s" {
+    It "returns multiple stderr lines from the native command as an array" {
         # Act
-        $stderr = @()
-        Invoke-NativeCommand $output --stderr,1,--stderr,2 -Stderr ([ref]$stderr)
+        $result = Invoke-NativeCommand $output --stderr err1 --stderr err2
 
         # Assert
-        $stderr -is [array] | Should -BeTrue
-        $stderr.Count | Should -Be 2
-        $stderr[0] | Should -BeOfType [string]
-        $stderr[0] | Should -BeExactly "1"
-        $stderr[1] | Should -BeOfType [string]
-        $stderr[1] | Should -BeExactly "2"
+        $result.Stderr -is [array] | Should -BeTrue
+        $result.Stderr.Count | Should -Be 2
+        $result.Stderr[0] | Should -BeOfType [string]
+        $result.Stderr[0] | Should -BeExactly "err1"
+        $result.Stderr[1] | Should -BeOfType [string]
+        $result.Stderr[1] | Should -BeExactly "err2"
     }
 }
--- Invoke/Invoke-NativeCommand.ps1
+++ Invoke/Invoke-NativeCommand.ps1
@@ -4,33 +4,28 @@ function Invoke-NativeCommand {
         [Parameter(Mandatory, Position = 0)]
         [string] $LiteralPath,
 
-        [Parameter(Mandatory, Position = 1)]
-        [string[]] $Arguments,
-
-        [AllowEmptyCollection()]
-        [Parameter(Position = 2)]
-        [ValidateScript({ $_.Value -is [array] -and $_.Value.Count -eq 0 })]
-        [ref] $Stdout,
-
-        [AllowEmptyCollection()]
-        [Parameter(Position = 3)]
-        [ValidateScript({ $_.Value -is [array] -and $_.Value.Count -eq 0 })]
-        [ref] $Stderr
+        [Parameter(ValueFromRemainingArguments)]
+        [string[]] $Arguments
     )
 
-    $output = & $LiteralPath @Arguments 2>&1
-
-    if ($null -eq $output) {
-        return
+    $result = @{
+        ExitCode = 0
+        Stderr = @()
+        Stdout = @()
     }
+
+    $output = & $LiteralPath @Arguments 2>&1
+    $result.ExitCode = $LASTEXITCODE
 
     foreach ($line in $output) {
-        if ($null -ne $Stdout -and $line -is [string]) {
-            $Stdout.Value += $line
+        if ($line -is [string]) {
+            $result.Stdout += $line
         }
 
-        if ($null -ne $Stderr -and $line -is [System.Management.Automation.ErrorRecord]) {
-            $Stderr.Value += $line.TargetObject
+        if ($line -is [System.Management.Automation.ErrorRecord]) {
+            $result.Stderr += $line.TargetObject
         }
     }
+
+    return $result
 }
