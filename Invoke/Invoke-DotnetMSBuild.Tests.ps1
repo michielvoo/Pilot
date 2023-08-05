c17dae0d39ebf720584f3982b0d69549d72b5d66
Michiel van Oosterhout <michielvoo@users.noreply.github.com>
2023-08-05 22:59:46 +0300

    Partially implement Invoke-DotnetMSBuild

---
+++ Invoke/Invoke-DotnetMSBuild.Tests.ps1
@@ -0,0 +1,38 @@
+BeforeAll {
+    . $PSCommandPath.Replace(".Tests.ps1", ".ps1")
+    . (Join-Path $PSScriptRoot "Invoke-Dotnet.ps1")
+
+    Mock Invoke-Dotnet {}
+}
+
+Describe Invoke-DotnetMSBuild {
+    It "invokes dotnet with command msbuild and arguments" {
+        # Act
+        $parameters = @{
+            DetailedSummary = $true
+            GraphBuild = $true
+            IgnoreProjectExtensions = "a","b","c"
+            InputResultsCaches = "d","e","f"
+            Interactive = $true
+            IsolateProjects = "Message"
+        }
+        Invoke-DotnetMSBuild proj @parameters arg1 arg2
+
+        # Assert
+        Should -Invoke Invoke-Dotnet -ParameterFilter {
+            $Command | Should -BeExactly "msbuild"
+            $Arguments | Should -Be @(
+                "proj"
+                "-detailedSummary:True"
+                "-graphBuild:True"
+                "-ignoreProjectExtensions:a,b,c"
+                "-inputResultsCaches:d,e,f"
+                "-interactive:True"
+                "-isolateProjects:Message"
+                "arg1"
+                "arg2"
+            )
+            $true
+        }
+    }
+}
