ed0a81b648405c0330252d72a7211fdd4dec2502
Michiel van Oosterhout <michielvoo@users.noreply.github.com>
2023-08-08 23:29:04 +0300

    Export new invoke cmdlets

--- Invoke/Invoke.psm1
+++ Invoke/Invoke.psm1
@@ -1,2 +1,17 @@
 . (Join-Path $PSScriptRoot (Join-Path "Invoke-Dotnet" "Invoke-DotnetNugetPush.ps1"))
 Export-ModuleMember -Function Invoke-DotnetNugetPush
+
+. (Join-Path $PSScriptRoot (Join-Path "Invoke-Git" "Invoke-GitDescribe.ps1"))
+Export-ModuleMember -Function Invoke-GitDescribe
+
+. (Join-Path $PSScriptRoot (Join-Path "Invoke-Git" "Invoke-GitFetch.ps1"))
+Export-ModuleMember -Function Invoke-GitFetch
+
+. (Join-Path $PSScriptRoot (Join-Path "Invoke-Git" "Invoke-GitRevList.ps1"))
+Export-ModuleMember -Function Invoke-GitRevList
+
+. (Join-Path $PSScriptRoot (Join-Path "Invoke-Git" "Invoke-GitRevParse.ps1"))
+Export-ModuleMember -Function Invoke-GitRevParse
+
+. (Join-Path $PSScriptRoot (Join-Path "Invoke-Git" "Invoke-GitShow.ps1"))
+Export-ModuleMember -Function Invoke-GitShow
