38ebca5cd4644a9c19d99a820ea6b49e69cc381d
Michiel van Oosterhout <michielvoo@users.noreply.github.com>
2023-08-08 20:10:54 +0300

    Minimal implementation of Invoke-GitRevList

--- Invoke/Invoke-Git/Invoke-GitRevList.Tests.ps1
+++ Invoke/Invoke-Git/Invoke-GitRevList.Tests.ps1
@@ -9,7 +9,12 @@ Describe Invoke-GitRevList {
     It "invokes git with the rev-list command and any arguments" {
         # Act
         $parameters = @{
-            
+            MaxCount = 1
+            Skip = 2
+            Since = [datetime]::new(2006, 1, 2, 15, 4, 5)
+            SinceAsFilter = [datetime]::new(2007, 1, 2, 15, 4, 5)
+            Until = [datetime]::new(2008, 1, 2, 15, 4, 5)
+            Count = $true
         }
         Invoke-GitRevList commit1,commit2,commit3 @parameters
 
@@ -21,6 +26,13 @@ Describe Invoke-GitRevList {
                 "commit1"
                 "commit2"
                 "commit3"
+                "--max-count=1"
+                "--skip=2"
+                "--since=2006-01-02T15:04:05.0000000"
+                "--since-as-filter=2007-01-02T15:04:05.0000000"
+                "--until=2008-01-02T15:04:05.0000000"
+
+                "--count"
             ) | ForEach-Object {
                 $Arguments[$i++] | Should -BeExactly $_
             }
--- Invoke/Invoke-Git/Invoke-GitRevList.ps1
+++ Invoke/Invoke-Git/Invoke-GitRevList.ps1
@@ -14,10 +14,94 @@ function Invoke-GitRevList {
     param (
         # 
         [Parameter(Mandatory, Position = 0)]
-        [string[]]$Commits
+        [string[]]$Commits,
+
+        # Commit limiting
+
+        # Limit the number of commits to output.
+        [Parameter()]
+        [int]$MaxCount,
+
+        # Skip number commits before starting to show the commit output.
+        [Parameter()]
+        [int]$Skip,
+
+        # Show commits more recent than a specific date.
+        [Alias("After")]
+        [Parameter()]
+        [datetime]$Since,
+
+        # Show all commits more recent than a specific date. This visits all commits in the range,
+        # rather than stopping at the first commit which is older than a specific date.
+        [Parameter()]
+        [datetime]$SinceAsFilter,
+
+        # Show commits older than a specific date.
+        [Alias("Before")]
+        [Parameter()]
+        [datetime]$Until,
+
+        # ...
+
+        # History Simplification
+
+        # Bisection Helpers
+
+        # Commit Ordering
+
+        # Object Traversal
+
+        # Commit Formatting
+
+        # ...
+
+        # Print a number stating how many commits would have been listed, and suppress all other
+        # output.
+        [Parameter()]
+        [switch]$Count
     )
 
     $Arguments = $Commits
+
+    # Commit limiting
+
+    if ($null -ne $MaxCount) {
+        $Arguments += "--max-count=$MaxCount"
+    }
+
+    if ($null -ne $Skip) {
+        $Arguments += "--skip=$Skip"
+    }
+
+    if ($null -ne $Since) {
+        $Arguments += "--since=$($Since.ToString("o"))"
+    }
+
+    if ($null -ne $SinceAsFilter) {
+        $Arguments += "--since-as-filter=$($SinceAsFilter.ToString("o"))"
+    }
+
+    if ($null -ne $Until) {
+        $Arguments += "--until=$($Until.ToString("o"))"
+    }
+
+    # ...
+
+    # History Simplification
+
+    # Bisection Helpers
+
+    # Commit Ordering
+
+    # Object Traversal
+
+    # Commit Formatting
+
+    # ...
+
+    if ($Count) {
+        $Arguments += "--count"
+    }
 
     Invoke-Git "rev-list" @Arguments
 }
