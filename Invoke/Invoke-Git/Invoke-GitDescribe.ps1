67cae710aeaba3e227e5fbf0aa4ba68c57b18926
Michiel van Oosterhout <michielvoo@users.noreply.github.com>
2023-08-08 09:26:47 +0300

    Finish Invoke-GitDescribe cmdlet

--- Invoke/Invoke-Git/Invoke-GitDescribe.ps1
+++ Invoke/Invoke-Git/Invoke-GitDescribe.ps1
@@ -1,5 +1,7 @@
 . (Join-Path $PSScriptRoot "Invoke-Git.ps1")
 
+# https://git-scm.com/docs/git-describe
+
 # .SYNOPSIS
 # Give an object a human readable name based on an available ref.
 # .DESCRIPTION
@@ -10,59 +12,63 @@
 # commit to other git commands.
 # By default (without `-All` or `-Tags`) `Invoke-GitDescribe` only shows annotated tags.
 function Invoke-GitDescribe {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = "Commitish")]
     [OutputType([hashtable])]
     param (
         # Commit-ish object names to describe. Defaults to `HEAD` if omitted.
-        [Parameter(Mandatory, Position = 0)]
-        [ParameterSetName("Commitish")]
+        [Parameter(Mandatory, Position = 0, ParameterSetName = "Commitish")]
         [string]$Commitish,
-
-        # If the given object refers to a blob, it will be described as <commit-ish>:<path>, such
-        # that the blob can be found at <path> in the <commit-ish>, which itself describes the
-        # first commit in which this blob occurs in a reverse revision walk from `HEAD`.
-        [Parameter(Mandatory, Position = 0)]
-        [ParameterSetName("Blob")]
-        [string]$Blob,
 
         # Describe the state of the working tree. When the working tree matches `HEAD`, the output
         # is the same as `Invoke-GitDescribe HEAD`. If the working tree has local modification
         # `--dirty` is appended to it. If a repository is corrupt and Git cannot determine if there
-        # is a local modification, Git will error out, unless `-Broken` is given, which appends the
-        # suffix `--broken` instead.
-        [Alias("Broken")]
-        [Parameter(Mandatory, Position = 0)]
-        [ParameterSetName("Dirty")]
-        [switch]$Dirty,
+        # is a local modification, Git will error out.
+        [Parameter(Mandatory, ParameterSetName = "Dirty")]
+        [ValidateScript({ ($_ -is [bool] -and $_ -eq $true) -or $_ -is [string] })]
+        [object]$Dirty,
+
+        # Describe the state of the working tree. When the working tree matches `HEAD`, the output
+        # is the same as `Invoke-GitDescribe HEAD`. If the working tree has local modification
+        # `--dirty` is appended to it. If a repository is corrupt and Git cannot determine if there
+        # is a local modification, Git will append the suffix `--broken` instead.
+        [Parameter(Mandatory, ParameterSetName = "Broken")]
+        [ValidateScript({ ($_ -is [bool] -and $_ -eq $true) -or $_ -is [string] })]
+        [object]$Broken,
+
+        # If the given object refers to a blob, it will be described as <commit-ish>:<path>, such
+        # that the blob can be found at <path> in the <commit-ish>, which itself describes the
+        # first commit in which this blob occurs in a reverse revision walk from `HEAD`.
+        [Parameter(Mandatory, Position = 0, ParameterSetName = "Blob")]
+        [string]$Blob,
 
         # Instead of using only the annotated tags, use any ref found in `refs/` namespace. This
         # option enables matching any known branch, remote-tracking branch, or lightweight tag.
-        [Parameter()]
-        [ParameterSetName("Commitish")]
-        [ParameterSetName("Dirty")]
+        [Parameter(ParameterSetName = "Commitish")]
+        [Parameter(ParameterSetName = "Dirty")]
+        [Parameter(ParameterSetName = "Broken")]
         [switch]$All,
 
         # Instead of using only the annotated tags, use any tag found in `refs/tags` namespace.
         # This option enables matching a lightweight (non-annotated) tag.
-        [Parameter()]
-        [ParameterSetName("Commitish")]
-        [ParameterSetName("Dirty")]
+        [Parameter(ParameterSetName = "Commitish")]
+        [Parameter(ParameterSetName = "Dirty")]
+        [Parameter(ParameterSetName = "Broken")]
         [switch]$Tags,
 
         # Instead of finding the tag that predates the commit, find the tag that comes after the
         # commit, and thus contains it. Automatically implies `-Tags`.
-        [Parameter()]
-        [ParameterSetName("Commitish")]
-        [ParameterSetName("Dirty")]
+        [Parameter(ParameterSetName = "Commitish")]
+        [Parameter(ParameterSetName = "Dirty")]
+        [Parameter(ParameterSetName = "Broken")]
         [switch]$Contains,
 
         # Instead of using the default number of hexadecimal digits (which will vary according to
         # the number of objects in the repository with a default of 7) of the abbreviated object
         # name, use <n> digits, or as many digits as needed to form a unique object name. An <n>
         # of 0 will suppress long format, only showing the closest tag.
-        [Parameter()]
-        [ParameterSetName("Commitish")]
-        [ParameterSetName("Dirty")]
+        [Parameter(ParameterSetName = "Commitish")]
+        [Parameter(ParameterSetName = "Dirty")]
+        [Parameter(ParameterSetName = "Broken")]
         [int]$Abbrev,
 
         # Instead of considering only the 10 most recent tags as candidates to describe the input
@@ -119,6 +125,79 @@ function Invoke-GitDescribe {
         [Parameter()]
         [switch]$FirstParent
     )
+
+    $Arguments = @()
+
+    if ($Commitish) {
+        $Arguments += $Commitish
+    }
+    elseif ($null -ne $Dirty) {
+        if ($Dirty -is [string]) {
+            $Arguments += "--dirty=$Dirty"
+        }
+        else {
+            $Arguments += "--dirty"
+        }
+    }
+    elseif ($null -ne $Broken) {
+        if ($Broken -is [string]) {
+            $Arguments += "--broken=$Broken"
+        }
+        else {
+            $Arguments += "--broken"
+        }
+    }
+    else {
+        $Arguments += $Blob
+    }
+
+    if ($All) {
+        $Arguments += "--all"
+    }
+
+    if ($Tags) {
+        $Arguments += "--tags"
+    }
+
+    if ($Contains) {
+        $Arguments += "--contains"
+    }
+
+    if ($Abbrev) {
+        $Arguments += "--abbrev=$Abbrev"
+    }
+
+    if ($Candidates) {
+        $Arguments += "--candidates=$Candidates"
+    }
+
+    if ($ExactMatch) {
+        $Arguments += "--exact-match"
+    }
+
+    if ($XDebug) {
+        $Arguments += "--debug"
+    }
+
+    if ($Long) {
+        $Arguments += "--long"
+    }
+
+    if ($Match) {
+        $Arguments += "--match",$Match
+    }
+
+    if ($Exclude) {
+        $Arguments += "--exclude",$Exclude
+    }
+
+    if ($Always) {
+        $Arguments += "--always"
+    }
+
+    if ($FirstParent) {
+        $Arguments += "--first-parent"
+    }
 
     Invoke-Git "describe" @Arguments
 }
