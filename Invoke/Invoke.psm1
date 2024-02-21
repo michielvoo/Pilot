7c960aed8d0cdae1fb119a4d674ac5c4689e8093
Michiel van Oosterhout <git@michielvoo.net>
2024-02-21 23:16:08 +0300

    Do not export unused members from nested module

--- Invoke/Invoke.psm1
+++ Invoke/Invoke.psm1
@@ -1,8 +1,3 @@
-. (Join-Path $PSScriptRoot (Join-Path "Invoke-Dotnet" "Invoke-DotnetNugetPush.ps1"))
+. "$PSScriptRoot/Invoke-Dotnet/Invoke-DotnetNugetPush.ps1"
+
 Export-ModuleMember -Function Invoke-DotnetNugetPush
-
-. (Join-Path $PSScriptRoot (Join-Path "Invoke-Git" "Invoke-GitBranchList.ps1"))
-Export-ModuleMember -Function Invoke-GitDescribe
-
-. (Join-Path $PSScriptRoot (Join-Path "Invoke-Git" "Invoke-GitDescribe.ps1"))
-Export-ModuleMember -Function Invoke-GitFetch
