27d95a7f7a7d83b3d9cc3d64d9db7dbad4669c20
Michiel van Oosterhout <git@michielvoo.net>
2024-03-06 21:43:35 +0300

    Fix conversion to integer

--- Private/Get-ScmRevisionCount.ps1
+++ Private/Get-ScmRevisionCount.ps1
@@ -2,7 +2,7 @@ function Get-ScmRevisionCount {
     # Get a number stating how many commits precede HEAD
     $result = Invoke-GitRevList "HEAD" -Count
     if ($result.ExitCode -eq 0) {
-        return $result.Stdout / 1
+        return [System.Convert]::ToInt32($result.Stdout)
     }
 
     return -1
